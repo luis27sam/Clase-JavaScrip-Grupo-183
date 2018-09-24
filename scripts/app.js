b7e18362d7d06b19b2b289d32bafc2d2cde44d51
Luis Gabriel Samudio <luis27sam@hotmail.com>
2018-09-24 16:18:11 -0500

    Segunda Actualizacion JavaS

---
+++ scripts/app.js
@@ -0,0 +1,44 @@
+/*
+var numero1 = 10;
+var numero2 = 5;
+var resultado = numero1*numero2;
+
+console.log(resultado);
+
+console.log("hola Mundo");
+
+var nombre = 'Luis Gabriel Samudio';
+var profesion = " Desarrollador Full Stack"
+
+console.log(nombre+profesion);
+
+var tiene_mascota = true;
+console.log(tiene_mascota);
+
+var datos = ["juan",26,false,25,78,"Desarrollador"]
+console.log(datos[0]);
+console.log(datos[2]);
+
+var edad = 41
+
+if(edad>=18 && edad<=40){
+	console.log("eres un adulto joven");
+}else{
+	console.log("No eres adulto joven");
+}
+
+var dias = ["lunes","martes","miercoles","jueves","viernes","sabado","domingo"]
+
+for(var i=0;i<7;i++){
+	console.log(dias[i]);
+}
+*/
+
+function suma(numero1,numero2){
+	var resultado = numero1+numero2;
+	return resultado;// body...
+}
+
+var res = suma(88,55);
+console.log(res);
+
